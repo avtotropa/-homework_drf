a1288070637453a4c8aef0706aa94745a74c8c2f
autotropa <autotropa@yandex.ru>
2023-09-01 17:20:05 +0300

    Added group MODERATOR

--- studyhub/views/lesson.py
+++ studyhub/views/lesson.py
@@ -2,25 +2,26 @@ from rest_framework.generics import RetrieveAPIView, UpdateAPIView, CreateAPIVie
 from rest_framework.permissions import IsAuthenticated
 
 from studyhub.models import Lesson
+from studyhub.permissions import IsModerator
 from studyhub.serializers.lesson import LessonSerializer
 
 
 class LessonDetailView(RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonUpdateView(UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonListView(ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonCreateView(CreateAPIView):
--- users/models.py
+++ users/models.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'member', _('member')
+    MODERATOR = 'moderator', _('moderator')
 
 
 class User(AbstractUser):
@@ -11,6 +17,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=50, verbose_name='Телефон', **NULLABLE)
     city = models.CharField(max_length=50, verbose_name='Город', **NULLABLE)
     avatar = models.ImageField(upload_to='avatars/', verbose_name='Аватар', **NULLABLE)
+    roles = models.CharField(max_length=10, choices=UserRoles.choices, default=UserRoles.MEMBER)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
